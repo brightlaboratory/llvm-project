e2a642657efd81cea6f52a719e28e6619606ce55
Sanket Tavarageri <sanket.tavarageri@intel.com>
2020-11-17 01:01:02 -0800

    Adding different versions of vectorized code

--- polydl_rt/polydl_rt.c
+++ polydl_rt/polydl_rt.c
@@ -43,7 +43,188 @@ void polydl_lib_matmul_f32(
 #endif
 
 #ifdef USE_AVX512
+void polydl_lib_matmul_f32_ver2(
+	long long int M, long long int N, long long int K,
+	long long int A_stride, long long int B_stride, long long int C_stride,
+	float *A, float *B, float *C) {
+
+	__m512 vec_C = _mm512_setzero_ps();
+	__m512 vec_A;
+	__m512 vec_B;
+
+	__m512 vec_C_1 = _mm512_setzero_ps();
+	__m512 vec_C_2 = _mm512_setzero_ps();
+	__m512 vec_C_3 = _mm512_setzero_ps();
+
+	__m512 vec_A_1, vec_A_2, vec_A_3;
+	__m512 vec_A_k_1, vec_A_1_k_1, vec_A_2_k_1, vec_A_3_k_1;
+	__m512 vec_B_k_1;
+
+	int i, j, k;
+	for (i = 0; i < M; i += 4) {
+		for (j = 0; j < N; j += 16) {
+			vec_C = _mm512_load_ps((__m512*)&C[i*C_stride + j]);
+			vec_C_1 = _mm512_load_ps((__m512*)&C[(i + 1)*C_stride + j]);
+			vec_C_2 = _mm512_load_ps((__m512*)&C[(i + 2)*C_stride + j]);
+			vec_C_3 = _mm512_load_ps((__m512*)&C[(i + 3)*C_stride + j]);
+
+
+			for (k = 0; k < K; k += 2) {
+				/* Equivalent to:
+				C[i][j] += A[i][k]* B[k][j];
+				C[i][j+1] += A[i][k]* B[k][j+1];
+				C[i][j+2] += A[i][k]* B[k][j+2];
+
+				C[i+1][j] += A[i+1][k]* B[k][j];
+				C[i+1][j+1] += A[i+1][k]* B[k][j+1];
+				C[i+1][j+2] += A[i+1][k]* B[k][j+2];
+
+				C[i][j] += A[i][k+1]* B[k+1][j];
+				C[i][j+1] += A[i][k+1]* B[k+1][j+1];
+				C[i][j+2] += A[i][k+1]* B[k+1][j+2];
+
+
+				...*/
+				vec_A = _mm512_set1_ps(A[i*A_stride + k]);
+				vec_A_1 = _mm512_set1_ps(A[(i + 1)*A_stride + k]);
+				vec_A_2 = _mm512_set1_ps(A[(i + 2)*A_stride + k]);
+				vec_A_3 = _mm512_set1_ps(A[(i + 3)*A_stride + k]);
+				vec_B = _mm512_load_ps((__m512*)&B[k*B_stride + j]);
+
+				vec_A_k_1 = _mm512_set1_ps(A[i*A_stride + (k + 1)]);
+				vec_A_1_k_1 = _mm512_set1_ps(A[(i + 1)*A_stride + (k + 1)]);
+				vec_A_2_k_1 = _mm512_set1_ps(A[(i + 2)*A_stride + (k + 1)]);
+				vec_A_3_k_1 = _mm512_set1_ps(A[(i + 3)*A_stride + (k + 1)]);
+				vec_B_k_1 = _mm512_load_ps((__m512*)&B[(k + 1)*B_stride + j]);
+
+				vec_C = _mm512_add_ps(vec_C, _mm512_mul_ps(vec_A, vec_B));
+				vec_C_1 = _mm512_add_ps(vec_C_1, _mm512_mul_ps(vec_A_1, vec_B));
+				vec_C_2 = _mm512_add_ps(vec_C_2, _mm512_mul_ps(vec_A_2, vec_B));
+				vec_C_3 = _mm512_add_ps(vec_C_3, _mm512_mul_ps(vec_A_3, vec_B));
+
+				vec_C = _mm512_add_ps(vec_C, _mm512_mul_ps(vec_A_k_1, vec_B_k_1));
+				vec_C_1 = _mm512_add_ps(vec_C_1, _mm512_mul_ps(vec_A_1_k_1, vec_B_k_1));
+				vec_C_2 = _mm512_add_ps(vec_C_2, _mm512_mul_ps(vec_A_2_k_1, vec_B_k_1));
+				vec_C_3 = _mm512_add_ps(vec_C_3, _mm512_mul_ps(vec_A_3_k_1, vec_B_k_1));
+			}
+
+			_mm512_store_ps((__m512*)&C[i*C_stride + j], vec_C);
+			_mm512_store_ps((__m512*)&C[(i + 1)*C_stride + j], vec_C_1);
+			_mm512_store_ps((__m512*)&C[(i + 2)*C_stride + j], vec_C_2);
+			_mm512_store_ps((__m512*)&C[(i + 3)*C_stride + j], vec_C_3);
+		}
+	}
+}
+
+
 void polydl_lib_matmul_f32(
+	long long int M, long long int N, long long int K,
+	long long int A_stride, long long int B_stride, long long int C_stride,
+	float *A, float *B, float *C) {
+
+	__m512 vec_C;
+	__m512 vec_A;
+	__m512 vec_B;
+
+	__m512 vec_C_1, vec_C_2, vec_C_3, vec_C_4, vec_C_5, vec_C_6, vec_C_7;
+	__m512 vec_A_1, vec_A_2, vec_A_3, vec_A_4, vec_A_5, vec_A_6, vec_A_7;
+
+	int i, j, k;
+	for (i = 0; i < M; i += 8) {
+		for (j = 0; j < N; j += 16) {
+			vec_C = _mm512_load_ps((__m512*)&C[i*C_stride + j]);
+			vec_C_1 = _mm512_load_ps((__m512*)&C[(i + 1)*C_stride + j]);
+			vec_C_2 = _mm512_load_ps((__m512*)&C[(i + 2)*C_stride + j]);
+			vec_C_3 = _mm512_load_ps((__m512*)&C[(i + 3)*C_stride + j]);
+			vec_C_4 = _mm512_load_ps((__m512*)&C[(i + 4)*C_stride + j]);
+			vec_C_5 = _mm512_load_ps((__m512*)&C[(i + 5)*C_stride + j]);
+			vec_C_6 = _mm512_load_ps((__m512*)&C[(i + 6)*C_stride + j]);
+			vec_C_7 = _mm512_load_ps((__m512*)&C[(i + 7)*C_stride + j]);
+
+
+			for (k = 0; k < K; k++) {
+				vec_B = _mm512_load_ps((__m512*)&B[k*B_stride + j]);
+
+				vec_A = _mm512_set1_ps(A[i*A_stride + k]);
+				vec_A_1 = _mm512_set1_ps(A[(i + 1)*A_stride + k]);
+				vec_A_2 = _mm512_set1_ps(A[(i + 2)*A_stride + k]);
+				vec_A_3 = _mm512_set1_ps(A[(i + 3)*A_stride + k]);
+				vec_A_4 = _mm512_set1_ps(A[(i + 4)*A_stride + k]);
+				vec_A_5 = _mm512_set1_ps(A[(i + 5)*A_stride + k]);
+				vec_A_6 = _mm512_set1_ps(A[(i + 6)*A_stride + k]);
+				vec_A_7 = _mm512_set1_ps(A[(i + 7)*A_stride + k]);
+
+
+				vec_C = _mm512_add_ps(vec_C, _mm512_mul_ps(vec_A, vec_B));
+				vec_C_1 = _mm512_add_ps(vec_C_1, _mm512_mul_ps(vec_A_1, vec_B));
+				vec_C_2 = _mm512_add_ps(vec_C_2, _mm512_mul_ps(vec_A_2, vec_B));
+				vec_C_3 = _mm512_add_ps(vec_C_3, _mm512_mul_ps(vec_A_3, vec_B));
+				vec_C_4 = _mm512_add_ps(vec_C_4, _mm512_mul_ps(vec_A_4, vec_B));
+				vec_C_5 = _mm512_add_ps(vec_C_5, _mm512_mul_ps(vec_A_5, vec_B));
+				vec_C_6 = _mm512_add_ps(vec_C_6, _mm512_mul_ps(vec_A_6, vec_B));
+				vec_C_7 = _mm512_add_ps(vec_C_7, _mm512_mul_ps(vec_A_7, vec_B));
+			}
+
+			_mm512_store_ps((__m512*)&C[i*C_stride + j], vec_C);
+			_mm512_store_ps((__m512*)&C[(i + 1)*C_stride + j], vec_C_1);
+			_mm512_store_ps((__m512*)&C[(i + 2)*C_stride + j], vec_C_2);
+			_mm512_store_ps((__m512*)&C[(i + 3)*C_stride + j], vec_C_3);
+			_mm512_store_ps((__m512*)&C[(i + 4)*C_stride + j], vec_C_4);
+			_mm512_store_ps((__m512*)&C[(i + 5)*C_stride + j], vec_C_5);
+			_mm512_store_ps((__m512*)&C[(i + 6)*C_stride + j], vec_C_6);
+			_mm512_store_ps((__m512*)&C[(i + 7)*C_stride + j], vec_C_7);
+		}
+	}
+}
+
+void polydl_lib_matmul_f32_ver1(
+	long long int M, long long int N, long long int K,
+	long long int A_stride, long long int B_stride, long long int C_stride,
+	float *A, float *B, float *C) {
+
+	__m512 vec_C = _mm512_setzero_ps();
+	__m512 vec_A = _mm512_setzero_ps();
+	__m512 vec_B = _mm512_setzero_ps();
+
+	__m512 vec_C_1 = _mm512_setzero_ps();
+	__m512 vec_C_2 = _mm512_setzero_ps();
+	__m512 vec_C_3 = _mm512_setzero_ps();
+
+	__m512 vec_A_1, vec_A_2, vec_A_3;
+
+	int i, j, k;
+	for (i = 0; i < M; i += 4) {
+		for (j = 0; j < N; j += 16) {
+			vec_C = _mm512_load_ps((__m512*)&C[i*C_stride + j]);
+			vec_C_1 = _mm512_load_ps((__m512*)&C[(i + 1)*C_stride + j]);
+			vec_C_2 = _mm512_load_ps((__m512*)&C[(i + 2)*C_stride + j]);
+			vec_C_3 = _mm512_load_ps((__m512*)&C[(i + 3)*C_stride + j]);
+
+
+			for (k = 0; k < K; k++) {
+
+				vec_A = _mm512_set1_ps(A[i*A_stride + k]);
+				vec_A_1 = _mm512_set1_ps(A[(i + 1)*A_stride + k]);
+				vec_A_2 = _mm512_set1_ps(A[(i + 2)*A_stride + k]);
+				vec_A_3 = _mm512_set1_ps(A[(i + 3)*A_stride + k]);
+				vec_B = _mm512_load_ps((__m512*)&B[k*B_stride + j]);
+
+				vec_C = _mm512_add_ps(vec_C, _mm512_mul_ps(vec_A, vec_B));
+				vec_C_1 = _mm512_add_ps(vec_C_1, _mm512_mul_ps(vec_A_1, vec_B));
+				vec_C_2 = _mm512_add_ps(vec_C_2, _mm512_mul_ps(vec_A_2, vec_B));
+				vec_C_3 = _mm512_add_ps(vec_C_3, _mm512_mul_ps(vec_A_3, vec_B));
+			}
+
+			_mm512_store_ps((__m512*)&C[i*C_stride + j], vec_C);
+			_mm512_store_ps((__m512*)&C[(i + 1)*C_stride + j], vec_C_1);
+			_mm512_store_ps((__m512*)&C[(i + 2)*C_stride + j], vec_C_2);
+			_mm512_store_ps((__m512*)&C[(i + 3)*C_stride + j], vec_C_3);
+		}
+	}
+}
+
+
+void polydl_lib_matmul_f32_base(
 	long long int M, long long int N, long long int K,
 	long long int A_stride, long long int B_stride, long long int C_stride,
 	float *A, float *B, float *C) {
