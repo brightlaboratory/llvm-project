56e90003133bdebe3d9732721cc24a1660b80542
Sanket Tavarageri <sanket.tavarageri@intel.com>
2021-03-22 09:26:21 -0700

    Updating the script

--- polydl_rt/experiments/matmul/run_matmul.sh
+++ polydl_rt/experiments/matmul/run_matmul.sh
@@ -7,11 +7,10 @@ export LD_LIBRARY_PATH=/nfs_home/stavarag/work/software/barvinok/barvinok-0.41.2
 
 
 OUT=poly_perf.csv
-
-check_correctness=1
 PERF_DIR=perf_data
 TEMP=temp
 DATATYPESIZE=4
+POLYSCIENTIST=/nfs_home/stavarag/work/polyscientist/data_reuse_analyzer/polyscientist
 
 mkdir ${PERF_DIR}
 mkdir ${TEMP}
@@ -35,7 +34,7 @@ file=matmul_explicit_data_packing.c
 
 echo iters=$iters M1=$M1 N1=$N1 K1=$K1 M2_Tile=$M2_Tile N2_Tile=$N2_Tile K2_Tile=$K2_Tile M1_Tile=$M1_Tile N1_Tile=$N1_Tile K1_Tile=$K1_Tile file=$file
 
-config=${iters}_${M1}_${N1}_${K1}__${M2_Tile}_${N2_Tile}_${K2_Tile}_${M1_Tile}_${N1_Tile}_${K1_Tile}_${PARALLEL_LOOP}_${NUM_THREADS}
+config=${iters}_${M1}_${N1}_${K1}__${M2_Tile}_${N2_Tile}_${K2_Tile}_${M1_Tile}_${N1_Tile}_${K1_Tile}_${Step_M}_${Step_N}_${Step_K}_${PARALLEL_LOOP}_${NUM_THREADS}
 echo config: $config
 
 CONFIG_OUT=${PERF_DIR}/${file}_${M1}_${N1}_${K1}_${PARALLEL_LOOP}_${OUT}
@@ -64,6 +63,7 @@ echo "#define K1_Tile ${K1_Tile}" >> $WORKFILE
 
 python ../../microkernel_codeGenerator.py ${Step_M} ${Step_N} ${Step_K} 0 0 0
 cp output.c ../../
+cp output.c ${TEMP}
 
 EXPERIMENTS_DIR=$PWD
 (cd ../.. && make clean && make version_file=versions/matmul_explicit_data_packing.c MACROFLAGS="-Djit_variant -DM1=$M1 -DN1=$N1 -DK1=$K1 -DNUM_ITERS=$iters -DM2_Tile=${M2_Tile} -DN2_Tile=${N2_Tile} -DK2_Tile=${K2_Tile} -DM1_Tile=${M1_Tile} -DN1_Tile=${N1_Tile} -DK1_Tile=${K1_Tile} -DPARALLEL_${PARALLEL_LOOP}")
@@ -71,21 +71,26 @@ EXPERIMENTS_DIR=$PWD
 ../../matmul &> run_output
 GFLOPS=`cat run_output |  grep Real_GFLOPS |  cut -d= -f2`
 ERROR=`cat run_output | grep "inf-norm of comp. abs. error" | cut -d: -f 2`
+RELERROR=`cat run_output | grep "inf-norm of comp. rel. error" | cut -d: -f 2`
 echo GFLOPS: $GFLOPS
 echo ERROR: $ERROR
 
 
-cat ../versions/$file >> $WORKFILE
+cat ../../versions/$file >> $WORKFILE
 CACHE_CONFIG='L1 32768 L2 1048576 L3 40370176'
 DATATYPESIZE=4
 NUM_PROCS=${NUM_THREADS}$
 output_file=${EXPERIMENTS_DIR}/${WORKFILE}_ws_stats.csv
 
+export LD_LIBRARY_PATH=/nfs_home/stavarag/work/software/barvinok/barvinok-0.41.2_install/lib:/nfs_home/stavarag/work/software/barvinok/isl_install/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/nfs_home/stavarag/work/software/barvinok/barvinok-0.41.2_install/lib:/nfs_home/stavarag/work/software/barvinok/isl_install/lib:$LD_LIBRARY_PATH
+
+
 #../../../data_reuse_analyzer/polyscientist --input $EXPERIMENTS_DIR/$WORKFILE --parameters 'dummy : 50 ' --cachesizes "${CACHE_CONFIG}" --datatypesize $DATATYPESIZE --minout
 
-time ../../../data_reuse_analyzer/polyscientist --input $EXPERIMENTS_DIR/$WORKFILE --parameters 'dummy : 50 ' --cachesizes "${CACHE_CONFIG}" --datatypesize $DATATYPESIZE --minout --parallel_loops ${PARALLEL_LOOP} --numprocs ${NUM_PROCS} --sharedcaches L3 
+time ${POLYSCIENTIST} --input $EXPERIMENTS_DIR/$WORKFILE --parameters 'dummy : 50 ' --cachesizes "${CACHE_CONFIG}" --datatypesize $DATATYPESIZE --minout --parallel_loops ${PARALLEL_LOOP} --numprocs ${NUM_PROCS} --sharedcaches L3 
 
-{ echo -n "${config},${GFLOPS}," ;  cat ${output_file} ; }  >> ${CONFIG_OUT}
-echo  "${config},${ERROR}" >> ${META_CONFIG_OUT}
+output_file=${EXPERIMENTS_DIR}/${WORKFILE}_ws_stats.csv
+{ echo -n "${config},${ERROR},${RELERROR},${GFLOPS}," ;  cat ${output_file} ; }  >> ${CONFIG_OUT}
 
 
