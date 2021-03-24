38b37eb0264eaf46773f4519920630a0cb619a5e
Sanket Tavarageri <sanket.tavarageri@intel.com>
2021-03-24 04:13:02 -0700

    Choosing the average GFLOPS

--- polydl_rt/experiments/oneDNN/run_matmul.sh
+++ polydl_rt/experiments/oneDNN/run_matmul.sh
@@ -24,6 +24,6 @@ export OMP_NUM_THREADS=${NUM_THREADS}
 config="m${m}n${n}k${k}"
 
 #echo -n $config, >> ${OUT}
-GFLOPS=`$BENCHDNN --matmul --mode=p --cfg=f32 $config"_nconfigg" | grep "_nconfigg" | cut -d"," -f9`
+GFLOPS=`$BENCHDNN --matmul --mode=p --cfg=f32 $config"_nconfigg" | grep "_nconfigg" | cut -d"," -f11`
 echo -n ${config_num},$GFLOPS >> ${OUT}
 echo "" >> ${OUT}
