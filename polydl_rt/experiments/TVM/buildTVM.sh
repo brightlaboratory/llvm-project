de1bcc986bacb33200cbef17e2d7a079d3d2543d
Sanket Tavarageri <sanket.tavarageri@intel.com>
2021-03-26 02:59:42 -0700

    Adding AutoTVM related files

---
+++ polydl_rt/experiments/TVM/buildTVM.sh
@@ -0,0 +1,21 @@
+git clone --recursive https://github.com/apache/tvm tvm
+cd tvm
+#git checkout dbd0114011906fe713ddda599bcba33a33b1fb73
+git checkout tags/v0.7.0
+#git checkout v0.6.0
+git submodule init
+git submodule update
+mkdir build
+cp cmake/config.cmake build
+cd build
+sed -i 's/set(USE_LLVM OFF)/set(USE_LLVM ON)/g' config.cmake
+cmake .. -G Ninja
+ninja
+pip install numpy decorator attrs
+pip install tornado xgboost
+pip install cython
+cd ..
+pip install psutil
+cd python; python3 setup.py install; cd ..
+#cd topi/python; python3 setup.py install; cd ../..
+#(sudo apt-get install python3.5-dev)
---
+++ polydl_rt/experiments/TVM/export.sh
@@ -0,0 +1,4 @@
+source /nfs_home/stavarag/work/software/Python-3.7.1_install/venv/bin/activate 
+source /swtools/cmake/cmake_vars.sh 
+source /swtools/ninja/ninja_vars.sh 
+source /swtools/llvm/11.1.0/vars.sh 
