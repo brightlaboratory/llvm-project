a07ca0def4a42ffa6e465ed31e95e16f35097cfc
Sanket Tavarageri <sanket.tavarageri@intel.com>
2021-04-04 23:13:28 -0700

    Updating the oneDNN version to v2.2.

--- polydl_rt/experiments/oneDNN/build_oneDNN.sh
+++ polydl_rt/experiments/oneDNN/build_oneDNN.sh
@@ -1,7 +1,7 @@
 rm -rf oneDNN
 git clone https://github.com/oneapi-src/oneDNN.git oneDNN
 cd oneDNN
-git checkout tags/v2.1.2
+git checkout tags/v2.2
 mkdir -p build install
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=../install/ ..
